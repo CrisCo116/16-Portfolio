e6361fd66fc6ff2abfae397d73adf80f67d6b532
Chris <138630942+CrisCo116@users.noreply.github.com>
2024-01-12 20:09:34 -0800

    Progress

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import App from './App.jsx';
+import Error from './pages/Error.jsx';
+import Projects from './pages/Project.jsx';
+import Contact from './pages/Contact.jsx';
+import About from './pages/About.jsx';
+import Resume from './pages/Resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: '/Projects',
+        element: <Projects />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,26 @@
+export default function Resume() {
+    return (
+      <div className="resume-container">
+        <article>
+          <h1>Resume</h1>
+          <p>
+          I am close successfully finishing a full-stack web development course, becoming skilled in building websites from start to finish. Learned front-end technologies like React.js, back-end with Node.js, and database management using MongoDB and MySQL. Can now create dynamic and scalable web applications, collaborating well in teams.
+          </p>
+        </article>
+  
+        <article>
+          <img src="./imgs/Resume.png" alt="Resume" />
+        </article>
+  
+        <article>
+          <h2>Full-Stack Skills</h2>
+          <ul>
+            <li>Frontend: React, JavaScript, HTML, CSS</li>
+            <li>Backend: Node.js, Express</li>
+            <li>Database: MongoDB, MySQL</li>
+          </ul>
+        </article>
+      </div>
+    );
+  }
+  
